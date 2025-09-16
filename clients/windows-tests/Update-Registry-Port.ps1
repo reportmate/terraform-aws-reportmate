06abbb9a6e8dd1dcdbc928512d0bce192db3ba5f
Rod Christiansen <rchristiansen@ecuad.ca>
2025-09-15 22:23:17 -0700

    Deleted some tests and debugs files

--- clients/windows-tests/Update-Registry-Port.ps1
+++
@@ -1,4 +0,0 @@
-# Update ReportMate registry to use port 3003
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\ReportMate" -Name "ServerUrl" -Value "http://localhost:3003" -Force
-Write-Host "Updated ServerUrl to http://localhost:3003"
-Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\ReportMate"
